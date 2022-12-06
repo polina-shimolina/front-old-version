13471f8662db6698a9717d92016aa1a83a490889
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2022-12-07 00:11:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import React, {Component} from "react";
 import './App.css';
-
+import DocumentTitle from 'react-document-title'
+import Home from "./components/Home";
+import CurrntParking from "./components/CurrntParking";
+import About from "./components/About";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <DocumentTitle title = 'ParkingService'>
+        <BrowserRouter basename="/" >
+          <div>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about/">
+                <About />
+              </Route>
+              <Route exact path={`/parking/:id/`}>
+                <Header />
+
+                <CurrntParking/>
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+
+      </DocumentTitle>
   );
 }
+
 
 export default App;
